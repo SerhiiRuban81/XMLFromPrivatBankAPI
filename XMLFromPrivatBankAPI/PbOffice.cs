186afba6800718b90047687197d62e54d3d851e9
SerhiiRuban81 <ruban_sa@hotmail.com>
2021-04-09 11:47:29 +0300

    Add project files.

---
+++ XMLFromPrivatBankAPI/PbOffice.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace XMLFromPrivatBankAPI
+{
+    public class PbOffice
+    {
+        [XmlAttribute(AttributeName = "country")]
+        public string Country { get; set; }
+        [XmlAttribute(AttributeName = "state")]
+        public string State { get; set; }
+        [XmlAttribute(AttributeName = "city")]
+        public string City { get; set; }
+        [XmlAttribute(AttributeName = "index")]
+        public string Index { get; set; }
+        [XmlAttribute(AttributeName = "address")]
+        public string Address { get; set; }
+        [XmlAttribute(AttributeName = "phone")]
+        public string Phone { get; set; }
+        [XmlAttribute(AttributeName = "email")]
+        public string Email { get; set; }
+        [XmlAttribute(AttributeName = "name")]
+        public string Name { get; set; }
+    }
+    [XmlRoot(ElementName ="pboffice")]
+    
+    public class DataOffice
+    {
+        [XmlElement("pboffice")]
+        public List<PbOffice> Offices { get; set; } = new List<PbOffice>();
+    }
+}
---
+++ XMLFromPrivatBankAPI/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+using System.Text;
+using System.Collections.Generic;
+
+namespace XMLFromPrivatBankAPI
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string str = await GetData();
+            XmlSerializer serializer = new XmlSerializer(typeof(DataOffice));
+            DataOffice office = null;
+            using (MemoryStream ms = new MemoryStream(Encoding.Default.GetBytes(str)))
+            {
+               office =  serializer.Deserialize(ms) as DataOffice;
+            }
+            foreach(var pb in office.Offices)
+            {
+                Console.WriteLine($"{pb.City} {pb.Name}");
+            }
+        }
+
+        static async Task<string> GetData()
+        {
+            Console.WriteLine("Hello World!");
+            string str = "";
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage resp = await client.GetAsync("https://api.privatbank.ua/p24api/pboffice?city=Кривой Рог");
+                str = await resp.Content.ReadAsStringAsync();
+                //Console.WriteLine(str);
+            }
+            return str;
+        }
+    }
+}
